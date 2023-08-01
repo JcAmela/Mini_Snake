d0e0902f744be2827a66af6e7946a6c4092c715b
JcAmela <juancarlosamela@gmail.com>
2023-08-01 09:29:28 +0200

    reparando touchpad

--- touchControl.js
+++ touchControl.js
@@ -1,41 +1,53 @@
-// touchControl.js
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
 
-var touchStartX = 0;
-var touchStartY = 0;
+var xDown = null;                                                        
+var yDown = null;                                                        
 
-function handleTouchStart(e) {
-  touchStartX = e.touches[0].clientX;
-  touchStartY = e.touches[0].clientY;
-}
+function handleTouchStart(evt) {                                         
+    xDown = evt.touches[0].clientX;                                      
+    yDown = evt.touches[0].clientY;                                      
+};                                                
 
-function handleTouchMove(e) {
-  e.preventDefault();
-  if (!touchStartX || !touchStartY) {
-    return;
-  }
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
 
-  var xUp = e.touches[0].clientX;
-  var yUp = e.touches[0].clientY;
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
 
-  var xDiff = touchStartX - xUp;
-  var yDiff = touchStartY - yUp;
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
 
-  if (Math.abs(xDiff) > Math.abs(yDiff)) {
-    if (xDiff > 0) {
-      changeDirection({x: -1, y: 0});
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0) {
+            /* left swipe */ 
+            Game.direction = { x: -1, y: 0 };
+        } else {
+            /* right swipe */
+            Game.direction = { x: 1, y: 0 };
+        }                       
     } else {
-      changeDirection({x: 1, y: 0});
-    }                       
-  } else {
-    if (yDiff > 0) {
-      changeDirection({x: 0, y: -1});
-    } else { 
-      changeDirection({x: 0, y: 1});
-    }                                                                 
-  }
-  touchStartX = null;
-  touchStartY = null;                                           
-}
+        if (yDiff > 0) {
+            /* up swipe */ 
+            Game.direction = { x: 0, y: -1 };
+        } else { 
+            /* down swipe */
+            Game.direction = { x: 0, y: 1 };
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
 
-window.addEventListener('touchstart', handleTouchStart, false);
-window.addEventListener('touchmove', handleTouchMove, false);
+window.addEventListener('resize', function() {
+  if (window.innerWidth < 1100) {
+    document.getElementById('game-info-desktop').style.display = 'none';
+    document.getElementById('game-info-mobile').style.display = 'block';
+  } else {
+    document.getElementById('game-info-desktop').style.display = 'block';
+    document.getElementById('game-info-mobile').style.display = 'none';
+  }
+});
